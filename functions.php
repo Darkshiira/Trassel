07e37212a070ab3368185beacb76d5c78af36978
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-09-01 19:41:17 +0200

    Added a new function on wordpress; pricelist

--- functions.php
+++ functions.php
@@ -1,3 +1,33 @@
 <?php 
-// Writing comment
+function custom_post_type() {
+    $labels = array(
+        'name'               => _x('Pricelist', 'post type general name'),
+        'singular_name'      => _x('Price Item', 'post type singular name'),
+        'add_new'            => __('Add New', 'book'),
+        'add_new_item'       => __('Add New Price Item'),
+        'edit_item'          => __('Edit Price Item'),
+        'new_item'           => __('New Price Item'),
+        'all_items'          => __('All Price Items'),
+        'view_item'          => __('View Price Item'),
+        'search_items'       => __('Search Price Item'),
+        'not_found'          => __('No Price Item found'),
+        'not_found_in_trash' => __('No Price Item found in the Trash'),
+        'parent_item_colon'  => '',
+        'menu_name'          => 'Pricelist'
+    );
+
+    $args = array(
+        'labels'        => $labels,
+        'public'        => true,
+        'has_archive'   => true,
+        'menu_position' => 5,
+        'supports'      => array('title','custom-fields')
+        // Add more arguments as needed
+    );
+
+    register_post_type('custom_post', $args);
+}
+
+add_action('init', 'custom_post_type');
+
  ?>
