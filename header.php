054525f11ef809344159bdebd3e4124f731f8cbb
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-09-01 19:40:52 +0200

    Added some html and the menu

--- header.php
+++ header.php
@@ -1,3 +1,22 @@
-<?php 
-// Writing comment
- ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo('charset');?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php bloginfo('name') ; ?> | <?php is_front_page() ? bloginfo('description') : wp_title(''); ?> </title>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<header>
+		<h2><?= get_bloginfo('name') ?></h2>
+		<nav>
+			<?php
+			wp_nav_menu(array(
+				'theme_location' => 'primary',
+				'menu_class' => 'primary-menu',
+			));
+			?>
+		</nav>
+	</header>
