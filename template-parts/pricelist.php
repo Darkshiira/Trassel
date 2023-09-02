1b5a1401d4a16455b292bf22a9d73a186c71d40b
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-09-02 10:28:31 +0200

    Made the <th> dynamic and iterates based on that

--- template-parts/pricelist.php
+++ template-parts/pricelist.php
@@ -1,40 +1,50 @@
+<?php
+$args = array(
+    'post_type' => 'custom_post', 
+    'posts_per_page' => -1,
+);
 
-    <?php
-    $args = array(
-        'post_type' => 'custom_post', // Use the actual post type name you registered
-        'posts_per_page' => -1,
-    );
-    
-    $custom_query = new WP_Query($args);
-    ?>
+$custom_query = new WP_Query($args);
+?>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Method</th>
-                <th>Short hair</th>
-                <th>Long hair</th>
-                <th>Children</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while ($custom_query->have_posts()) :
+<table>
+    <thead>
+        <tr>
+            <th>Method</th>
+            <?php
+            
+            $custom_field_names = array();
+            while ($custom_query->have_posts()) :
                 $custom_query->the_post();
+                $custom_fields = get_post_custom();
+                foreach ($custom_fields as $field_name => $field_values) {
+                    if (in_array($field_name, array('_edit_lock', '_edit_last', '_thumbnail_id'))) {
+                        continue;
+                    }
+                    if (!in_array($field_name, $custom_field_names)) {
+                        $custom_field_names[] = $field_name;
+                        echo '<th>' . esc_html($field_name) . '</th>';
+                    }
+                }
+            endwhile;
             ?>
-            <tr>
-                <td><?php the_title(); ?></td>
-                <?php
-                // Get custom field values for the current post
-                $short_hair = get_post_meta(get_the_ID(), 'short_hair', true);
-                $long_hair = get_post_meta(get_the_ID(), 'long_hair', true);
-                $children = get_post_meta(get_the_ID(), 'children', true);
-                ?>
-                <td><?php echo esc_html($short_hair); ?></td>
-                <td><?php echo esc_html($long_hair); ?></td>
-                <td><?php echo esc_html($children); ?></td>
-            </tr>
-            <?php endwhile; ?>
-        </tbody>
-    </table>
+        </tr>
+    </thead>
+    <tbody>
+        <?php while ($custom_query->have_posts()) :
+            $custom_query->the_post();
+        ?>
+        <tr>
+            <td><?php the_title(); ?></td>
+            <?php
+            foreach ($custom_field_names as $field_name) {
+                $field_value = get_post_meta(get_the_ID(), $field_name, true);
+                echo '<td>' . esc_html($field_value) . '</td>';
+            }
+            ?>
+        </tr>
+        <?php endwhile; ?>
+    </tbody>
+</table>
 
-    <?php wp_reset_postdata(); ?>
+<?php wp_reset_postdata(); ?>
