e6a0dd973f20b9c1854818c132186c7fb1a2bef9
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-09-04 08:06:17 +0200

    If is home inlagd

--- index.php
+++ index.php
@@ -4,9 +4,11 @@ get_header(); ?>
 <main id="main">
     <?php
     
+    if ( is_home() &&  is_front_page() ) : 
     get_template_part('template-parts/pricelist', 'pricelist');
 
     echo do_shortcode('[CP_APP_HOUR_BOOKING id="2"]');
+    endif;
 
     while ( have_posts() ) :
         the_post();
