cc68baae494d33f12305b39cd7d0c3892ac007d3
Hanna Arefiardakani <darkshiirarhotmail.com>
2023-09-05 13:24:21 +0200

    removed hardcoded widget

--- index.php
+++ index.php
@@ -5,11 +5,6 @@ get_header(); ?>
     <div id=homepage>
         <div id="main-feed">
             <?php
-    
-                 if ( is_home() &&  is_front_page() ) : 
-                     get_template_part('template-parts/pricelist', 'pricelist');
-
-                 endif;
 
                  while ( have_posts() ) :
                     the_post();
